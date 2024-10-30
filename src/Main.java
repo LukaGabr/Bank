32ce55b5f3089e90ca44a89aee9b2ae1ed09c56f
LukaGabr(Woodstock) <lluukkaa.gabrielashvili@gmail.com>
2024-10-30 16:10:14 -0400

    Created new account for customerOne

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         // Customer One
-        PhysicalCard customerOne_PhysicalCardOne =new PhysicalCard
+        PhysicalCard customerOne_PhysicalCardOne = new PhysicalCard
                 ("2342 8655 4441 1233", "10-Nov-2025", "Debit", 334);
-        PhysicalCard customerOne_PhysicalCardTwo =new PhysicalCard
+        PhysicalCard customerOne_PhysicalCardTwo = new PhysicalCard
                 ("8965 0904 4324 3433", "03-May-2026", "Credit", 531);
 
         Account customerOne_AccountOne = new Account
@@ -20,12 +20,20 @@ public class Main {
                 ("2341 4432 1122 0931", "30-Dec-2027", "Credit", 119);
 
         Account customerTwo_AccountOne = new Account
-                (113532679, 245.45f, "Checking", customerTwo_PhysicalCardOne);
+                (113532680, 245.45f, "Checking", customerTwo_PhysicalCardOne);
 
         Customer customerTwo = new Customer
                 ("Jimi", "Hendrix", "27-Nov-1942", customerTwo_AccountOne);
 
+        // Creating a Physical card and an Account for accountOne
+        PhysicalCard customerOne_PhysicalCardThree = new PhysicalCard
+                ("4443 2321 1123 0909", "11-Aug-2025", "Debit", 321);
 
+        Account customerOne_AccountThree = new Account
+                (113532679, 20f, "Checking", customerOne_PhysicalCardThree);
+
+        // Adding third account to customerOne
+        customerOne.addAccount(customerOne_AccountThree);
         System.out.println("**********************************************\n");
         System.out.println(customerOne);
         System.out.println("**********************************************\n");
